934e9eb0376aec7f8756955f5309f7ea528e0cfb
jiyeonnn03 <yeoni0324@nate.com>
2025-02-06 14:59:39 +0900

    Modify fin.py - 참고자료 출처 추가

--- fin.py
+++ fin.py
@@ -1,4 +1,7 @@
+import sys
 import os
+import json
+
 from dotenv import load_dotenv
 from openai import OpenAI
 from sentence_transformers import SentenceTransformer
@@ -56,7 +59,7 @@ def analyze_financial(stock_name):
 
     # OpenAI GPT-4 호출
     response = client.chat.completions.create(
-        model="gpt-4",
+        model="gpt-4o",
         messages=[
             {"role": "system",
              "content": "당신은 개인 투자자들이 특정 종목에 대한 재무 데이터를 이해하고 투자 결정을 내릴 수 있도록 돕는 금융 분석 전문가입니다."},
@@ -70,11 +73,21 @@ def analyze_financial(stock_name):
     print("\nGenerated Result:\n")
     print(result)
 
-    return result
+    json_output = json.dumps(
+        {
+            "result_fin": result,
+            "reference": {
+                'title': '전자공시시스템',
+                'description': '(DART : Data Analysis, Retrieval and Transfer System)의 OpenDART 재무정보조회 데이터에 근거한 분석 결과입니다. ',
+                'url': 'https://dart.fss.or.kr/'
+            }
+        },
+        ensure_ascii=False, indent=4)
+
+    print(json_output)
 
 
 if __name__ == "__main__":
-    # 테스트용 종목 이름 설정
-    stock_name = '삼성전자'
-    # 재무 분석 실행
+    stock_name = sys.argv[1]
+    # stock_name = '삼성전자'
     analyze_financial(stock_name)
